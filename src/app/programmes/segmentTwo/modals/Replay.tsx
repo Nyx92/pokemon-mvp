aefc1058df6376034c5f28bb7008d5ddacb42d84
Nyx92 <ngyx92@gmail.com>
2024-09-27 00:32:41 +0800

    la

--- src/app/programmes/segmentTwo/modals/Replay.tsx
+++ src/app/programmes/segmentTwo/modals/Replay.tsx
@@ -44,7 +44,7 @@ function StartedModal({ open, onClose }) {
         </IconButton>
         {/* First sentence */}
         <Typography sx={{ fontSize: "15px", fontWeight: "bold", mb: 2 }}>
-          Key Pillars
+          Dance Science and Health (daSH) Programmes
         </Typography>
         {/* Second sentence */}
         <Typography
@@ -54,7 +54,7 @@ function StartedModal({ open, onClose }) {
             mb: 2,
           }}
         >
-          Care.
+          Re:play
         </Typography>
 
         {/* First Card */}
@@ -77,41 +77,79 @@ function StartedModal({ open, onClose }) {
                   fontSize: { xs: "15px", sm: "20px" },
                   fontWeight: "bold",
                   mb: 2,
+                  textAlign: "justify", // Justifies the text for alignment
                 }}
               >
-                Caring for all.
+                For our seniors.
                 <span style={{ color: "#6E6E73" }}>
-                  {"  "}Caught in the hustle and bustle of our time scarce
-                  society, we experience a gradual erosion of care. A sincere,
-                  attentive presence is often hard to come by. We recognize the
-                  value of collective artmaking and creative practice as sites
-                  for restoring care. The experience of dancing and moving
-                  together — engaged joyously in a creative endeavour — creates
-                  openings for mutual relating, nurtures connections between
-                  people through the relational qualities of touch, which in the
-                  process, allows us to enact gestures of care. We offer
-                  invitations to come into more care-full ways of relating to
-                  one another, paving ways for more equitable, just and caring
-                  societies.
+                  {"  "}The artistic freedom and social aspects of dance can
+                  help seniors manage the stress and challenges to daily living
+                  due to conditions like Dementia or Parkinson’s. Dancing can
+                  enhance fluidity of movement, postural stability, flexibility
+                  of the spine and improve balance. The use of rhythm and voice
+                  can help with cueing movement and expression. The integration
+                  of music and dance into reminiscence therapy also helps people
+                  with dementia evoke memories and stimulate mental activity.
+                  <br></br>
+                  <br></br>
                 </span>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center", // Add this line to center conte
+                  }}
+                >
+                  {/* Add an img tag here with src set to the image path */}
+                  <img
+                    src="/about/care_about.jpg" // Change to your image path
+                    alt="heart"
+                    style={{
+                      borderRadius: "20px",
+                      width: "30%",
+                    }}
+                  />
+                </Box>
+                CORE. Creating Opportunities, Resources and Empowerment for a
+                Community Of Ready Elders!
+                <span style={{ color: "#6E6E73" }}>
+                  {"  "} Definitely a mouthful but the idea is straightforward —
+                  a programme to empower seniors to train other seniors in
+                  movement and wellness activities.
+                  <br></br>
+                  <br></br>
+                  Evidence-based dance programmes have been shown to improve the
+                  functional health of participants and also lead to sustained
+                  improvement to psychosocial states. Studies have found that:
+                  <br></br>
+                  <br></br>
+                  Frequent dance activity is associated with a 76% reduced risk
+                  of dementia, the greatest reduction compared to other
+                  activities like reading, swimming, or doing crossword puzzles
+                  frequently Dancing at moderate intensities was associated with
+                  a reduced risk of cardiovascular disease (more so than
+                  walking!) Dance-based mind-motor interventions were associated
+                  with a significant reduction in rate of falls among older
+                  adults
+                </span>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center", // Add this line to center conte
+                  }}
+                >
+                  {/* Add an img tag here with src set to the image path */}
+                  <img
+                    src="/about/care_about.jpg" // Change to your image path
+                    alt="heart"
+                    style={{
+                      borderRadius: "20px",
+                      width: "30%",
+                    }}
+                  />
+                </Box>
               </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center", // Add this line to center conte
-              }}
-            >
-              {/* Add an img tag here with src set to the image path */}
-              <img
-                src="/about/care_about.jpg" // Change to your image path
-                alt="heart"
-                style={{
-                  borderRadius: "20px",
-                  width: "30%",
-                }}
-              />
             </Box>
           </CardContent>
         </Card>
