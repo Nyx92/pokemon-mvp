da39300fd03f8afca3dc8290504ee08c62973899
Nyx92 <ngyx92@gmail.com>
2025-10-18 17:22:38 +0800

    testing ui

--- src/app/profile/edit/general/EditProfilePage.tsx
+++ src/app/profile/edit/general/EditProfilePage.tsx
@@ -137,6 +137,7 @@ export default function EditProfilePage({ initialUser }: EditProfilePageProps) {
         pb: 10,
         display: "flex",
         flexDirection: "column",
+        backgroundColor: "yellow",
       }}
     >
       {/* Keep title fixed at the top */}
