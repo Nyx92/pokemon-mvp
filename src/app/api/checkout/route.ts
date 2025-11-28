4da18d95f12371c05520c32133f254376efa2880
Nyx92 <ngyx92@gmail.com>
2025-11-29 03:44:59 +0800

    fix stripe

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2025-02-24.acacia",
+});
 
 export async function POST(req: NextRequest) {
   try {
